730d6d72a79b10f2761d21f54747c9fc9d4c6331
reesehu <1101121039@qq.com>
2016-10-13 17:22:29 +0800

    fixbug

--- lightguideviewlib/src/main/java/com/reesehu/lightguideviewlib/util/DebugLog.java
+++ lightguideviewlib/src/main/java/com/reesehu/lightguideviewlib/util/DebugLog.java
@@ -5,13 +5,16 @@ import android.util.Log;
 /**
  * Created by zhy on 15/9/23.
  */
-public class L
-{
+public class DebugLog {
     private static final String TAG = "HighLight";
     private static boolean debug = true;
 
-    public static void e(String msg)
-    {
+    public static void e(String msg) {
+        if (debug)
+            Log.e(TAG, msg);
+    }
+
+    public static void d(String msg) {
         if (debug)
             Log.e(TAG, msg);
     }
